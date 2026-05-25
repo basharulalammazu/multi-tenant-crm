4cfeac59d275d94ac9af1a9e80c0885663a749a7
basharulalammazu <basharulalam975@gmail.com>
2026-05-25 20:24:39 +0600

    Data Access Factory added

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,72 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        private CrmSaaSDbContext db;
+
+        public DataAccessFactory(CrmSaaSDbContext db)
+        {
+            this.db = db;
+        }
+
+        // Generic repository access
+        public IRepository<T> GetRepo<T>() where T : class
+        {
+            return new Repository<T>(db);
+        }
+
+        // Feature specific repository
+        public IActivityRepo ActivityRepoAccess()
+        {
+            return new ActivityRepo(db);
+        }
+
+        public IAppUserRepo AppUserRepoAccess()
+        {
+            return new AppUserRepo(db);
+        }
+
+        public IAuditLogRepo AuditLogRepoAccess()
+        {
+            return new AuditLogRepo(db);
+        }
+
+        public IContactNoteRepo ContactNoteRepoAccess()
+        {
+            return new ContactNoteRepo(db);
+        }
+
+        public IContactRepo ContactRepoAccess()
+        {
+            return new ContactRepo(db);
+        }
+
+        public IDealRepo DealRepoAccess()
+        {
+            return new DealRepo(db);
+        }
+
+        public ILeadRepo LeadRepoAccess()
+        {
+            return new LeadRepo(db);
+        }
+
+        public INotificationRepo NotificationRepoAccess()
+        {
+            return new NotificationRepo(db);
+        }
+
+        public ITenantRepo TenantRepoAccess()
+        {
+            return new TenantRepo(db);
+        }
+
+    }
+}
